7d752fc787bff0c1b6b6b55d09ec4764aab811e5
ShannonPaige <spaige@gmail.com>
2015-09-28 20:14:20 -0600

    done with TDD

--- test/webserver_test.rb
+++ test/webserver_test.rb
@@ -2,14 +2,27 @@ require_relative '../lib/http_yeah_you_know_me'
 require 'stringio'
 
 class HttpYeahYouKnowMeTest < Minitest::Test
-  def test_it_reads_exact_length_of_body
-    client = StringIO.new("POST /to_braille HTTP/1.1\r\n" +
+  def setup
+    @client = StringIO.new("POST /to_braille HTTP/1.1\r\n" +
              "Content-Length: 10\r\n" +
              "\r\n" +
              "0123456789Do not read this")
+    @env_hash = Parse.call(@client)
+  end
 
-    env_hash = Parse.call(client)
-    assert_equal "0123456789", env_hash["rack.input"].read
+  def test_it_reads_exact_length_of_body
+    assert_equal "0123456789", @env_hash["rack.input"].read
+  end
 
+  def test_it_stores_the_first_word_in_the_REQUEST_METHOD_key
+    assert_equal "POST", @env_hash["REQUEST_METHOD"]
+  end
+
+  def test_it_stores_the_second_word_in_the_PATH_INFO_key
+    assert_equal "/to_braille", @env_hash["PATH_INFO"]
+  end
+
+  def test_it_stores_the_third_word_in_the_VERSION_key
+    assert_equal "HTTP/1.1", @env_hash["VERSION"]
   end
 end
